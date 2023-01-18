5c2e1f727cd01345d32e96afe79e772e998d4e90
Nestor <nlzx432@gmail.com>
2023-01-17 22:52:53 -0500

    add example db use

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -1,8 +1,41 @@
 package edu.wpi.teamname;
 
+import java.sql.*;
+
 public class Main {
 
   public static void main(String[] args) {
-    App.launch(App.class, args);
+    //    App.launch(App.class, args);
+    Connection connection = null;
+
+    try {
+      Class.forName("org.postgresql.Driver");
+    } catch (ClassNotFoundException e) {
+      throw new RuntimeException(e);
+    }
+
+    try {
+      connection =
+          DriverManager.getConnection(
+              "jdbc:postgresql://localhost:5432/postgres", "postgres", "password");
+
+      connection
+          .createStatement()
+          .executeUpdate("CREATE TABLE stuff(name VARCHAR, value VARCHAR);");
+
+      connection
+          .createStatement()
+          .executeUpdate("INSERT INTO stuff(name, value) VALUES('thing1', '42');");
+
+      ResultSet rs2 = connection.createStatement().executeQuery("SELECT * FROM stuff;");
+      while (rs2.next()) {
+        System.out.print(rs2.getString(1));
+        System.out.print(" --- ");
+        System.out.println(rs2.getString(2));
+      }
+
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
